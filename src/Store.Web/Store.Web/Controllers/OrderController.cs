b7db59f7d3728b596110f512dad92dfc3d032901
Igor Araújo <igor.araujo@squadra.com.br>
2020-09-25 19:05:12 -0300

    feat: add controller and view

--- src/Store.Web/Store.Web/Controllers/OrderController.cs
+++ src/Store.Web/Store.Web/Controllers/OrderController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web;
+﻿using Store.Web.StoreWS;
+using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Store.Web.Controllers
@@ -13,10 +16,46 @@ namespace Store.Web.Controllers
         // GET: Order
         public ActionResult Import(HttpPostedFileBase file)
         {
-            //var client = new ServiceSoapClient();
-            //client.Example();
+            if (file == null || file.ContentLength == 0)
+            {
+                ViewBag.StatusClass = "alert alert-warning";
+                ViewBag.StatusMessage = "O campo não pode estar vazio.";
+
+                return View();
+            }
+
+            var filePath = Path.Combine(Server.MapPath("~/TempImportFiles"), Path.GetFileName(file.FileName));
+
+            try
+            {
+                file.SaveAs(filePath);
+
+                var client = new ServiceSoapClient();
+                client.ImportFile(filePath);
+
+                ViewBag.StatusClass = "alert alert-success";
+                ViewBag.StatusMessage = "Arquivo importado com sucesso.";
+
+            }
+            catch (Exception e)
+            {
+                string message = InnerException(e).Message;
+
+                ViewBag.StatusClass = "alert alert-danger";
+                ViewBag.StatusMessage = $"Erro ao importar o arquivo. Erro: {message}";
+            }
 
             return View();
+        }
+
+        private Exception InnerException(Exception e)
+        {
+            if (e.InnerException != null)
+            {
+                InnerException(e.InnerException);
+            }
+
+            return e;
         }
     }
 }
